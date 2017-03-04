1383773908d9dd846e44f31851d6d2021d20ef02
Slava <vandernyt23@gmail.com>
2017-03-04 15:57:40 +0200

    Update User.cpp

--- coding-style/cpp/User.cpp
+++ coding-style/cpp/User.cpp
@@ -1,16 +1,25 @@
 #include "pch.h"
+
 #include "User.h"
 
-namespace Db
-{
+namespace Db {
+
+//
+// Definition of the User class.
+//
 
 User::User(const std::string& name, const std::string& email,
 	const std::string& pwd, const Role role)
-	: name_(name)
-	, email_(email)
-	, pwd_(pwd)
-	, role_(std::make_pair(role, static_cast<int>(role)))
+	: name_{ name }
+	, email_{ email }
+	, pwd_{ pwd }
+	, role_{ std::make_pair(role, static_cast<int>(role)) }
 {
+}
+
+void User::Log() const
+{
+	// TODO: Just for example.
 }
 
 } // namespace Db
